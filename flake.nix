272fd46ce19d1670fd4aa9c45a69b6b038eb720b
comfybyte <comfybyte@proton.me>
2023-09-21 02:26:22 -0300

    fix: remove nixos cache because holy moly am i on mars or something

--- flake.nix
+++ flake.nix
@@ -33,14 +33,14 @@
 
   nixConfig = {
       substituters = [
-        "https://cache.nixos.org"
+        # "https://cache.nixos.org"
         "https://hyprland.cachix.org"
         "https://nixpkgs-wayland.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        # "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
