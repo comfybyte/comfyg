9d854a728857688940ec020e3bea7b56802950b0
mayaneru <mayaneru@proton.me>
2023-11-13 11:38:05 -0300

    kirisame: change to kernel_zen

--- hosts/kirisame/default.nix
+++ hosts/kirisame/default.nix
@@ -15,7 +15,7 @@
   };
   nixpkgs.config.allowUnfree = true;
 
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_6;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
   boot.kernel.sysctl."fs.inotify.max_user_watches" = 1048576;
   boot.kernel.sysctl."kernel.sysrq" = 1;
   # Usa o systemd-boot como bootloader.
